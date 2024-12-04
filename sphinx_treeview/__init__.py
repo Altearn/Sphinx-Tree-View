3f443144844050fe32a379d9fd205b5cee4f1517
Théo Giraudet <theo.giraudet@orange.fr>
2024-12-04 19:29:07 +0100

    Add readme to toml file

--- sphinx_treeview/__init__.py
+++ sphinx_treeview/__init__.py
@@ -10,7 +10,7 @@ from jinja2 import Environment, FileSystemLoader
 
 logger = logging.getLogger(__name__)
 
-version = "0.1.0"
+__version__ = "0.1.1"
 
 class DectoratorRegistry(Enum):
     DIR = "dir"
